3f36f9aa8939b3c32411492dab7eb6aa413e34a7
JeanValverde24 <jv2020066920@virtual.upt.pe>
2024-12-07 12:28:41 -0500

    version 1.1.19

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="TeamWebQaUPT",
-    version="1.1.18",
+    version="1.1.19",
     description="Paquete para pruebas automatizadas de interfaces web",
     long_description=long_description,
     long_description_content_type="text/markdown",
